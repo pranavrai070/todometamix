0f3e00de7524dd1b543509742520566101fb99c6
pranavrai070 <pranavrai070@gmail.com>
2022-12-07 18:34:46 +0530

    structure made

---
+++ server/models/postMessage.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const postSchema=mongoose.Schema({
+   title:String,
+   task:String,
+   creator:String,
+   tags:[String],
+   selectedFile:String,
+   createdAt:{
+    type:Date,
+    default:new Date()
+   }
+});
+
+const PostMessage=mongoose.model('PostMessage',postSchema);
+
+export default PostMessage;
